bdd0430b61d4f483129f0a6c93c020f072294988
Ian Caragol <iancaragol@gmail.com>
2018-10-26 11:27:01 -0600

    Added stats tracked and command

--- DiscordBotTor/DiscordBotTor/client.py
+++ DiscordBotTor/DiscordBotTor/client.py
@@ -8,6 +8,10 @@
 #https://discordapp.com/oauth2/authorize?client_id=505120997658329108&scope=bot&permissions=75776
 
 
+token = "NTAwNzMzODQ1ODU2MDU5NDAy.DqPI-g.BBe4LLMfN3QFmrZXOK3mTNwSpws"
+testToken = "NTA1MTIwOTk3NjU4MzI5MTA4.DrO-Lg.jiyCbUD07MoplIKMq01kBSRbaPs"
+
+
 import BookOfTor
 import DiceRolls
 import MonsterManual
@@ -20,6 +24,8 @@ import time
 import math
 import re
 import random
+import json
+import os
 
 from discord.voice_client import VoiceClient
 
@@ -27,8 +33,43 @@ class MyClient(discord.Client):
     def __init__(self):
         self.initDict = {}
         self.initEmbedDict = {}
+        
+        try:
+            pyDir = os.path.dirname(__file__)
+            relPath = "_data//stats.txt"
+            absRelPath = os.path.join(pyDir, relPath)
+            self.statsDict = json.load(open(absRelPath))
+
+        except Exception:
+            self.statsDict = {'!tor horo':0, '!tor zodiac':0, '!hello':0, '!tor styles':0, '!tor randchar':0, '!roll':0,
+                          '!help':0, '!mm':0, '!randmonster':0, '!feat':0, '!randfeat':0, '!start init':0, '!add init':0}
+
+        self.userSet = set()
+
         return super().__init__()
-              
+            
+    async def displayStats(self):
+        retStr = f'''**Lifetime Stats**
+   > !help: {self.statsDict['!help']}
+   > !hello: {self.statsDict['!hello']}
+   > !start init: {self.statsDict['!start init']}
+   > !add init: {self.statsDict['!add init']}
+   > !roll: {self.statsDict['!roll']}
+
+   *D&D 5E Specific Commands:*
+   > !feat: {self.statsDict['!feat']}
+   > !mm: {self.statsDict['!mm']}   
+   > !randfeat: {self.statsDict['!randfeat']}
+   > !randmonster: {self.statsDict['!randmonster']}
+
+   *Book of Tor Specific Commands:*
+   > !tor horo: {self.statsDict['!tor horo']}
+   > !tor randchar: {self.statsDict['!tor randchar']}
+   > !tor styles: {self.statsDict['!tor styles']}
+   > !tor zodiac: {self.statsDict['!tor zodiac']}
+   '''
+        return retStr
+
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
@@ -36,39 +77,39 @@ class MyClient(discord.Client):
         print('------')
     
     async def on_message(self, message):
-        #ts = time.gmtime()
-        #t = time.strftime("%Y-%m-%d %H:%M:%S", ts)
-        #print(f"Received command: {message.content.lower()} from {message.author} at {t}")   
-
         # don't respond to ourselves
         if message.author == self.user:
             return
 
         if message.content.lower().startswith('!tor horo'):
+            self.statsDict['!tor horo'] += 1
             await message.channel.send(f"<@{message.author.id}>" + "\n" + await bt.horo())
 
         if message.content.lower().startswith('!tor zodiac'):
+            self.statsDict['!tor zodiac'] += 1
             await message.channel.send(f"<@{message.author.id}>" + "\n" + await bt.zodiac())
 
         if message.content.lower().startswith('!hello'):
+            self.statsDict['!hello'] += 1
             embed = discord.Embed()
             embed.set_image(url='https://cdn.discordapp.com/attachments/352281669992185866/500780935638155264/kOXnswR.gif')
 
             await message.channel.send(embed=embed)
 
         if message.content.lower().startswith('!tor styles'):
+            self.statsDict['!tor styles'] += 1
             await message.channel.send(f"<@{message.author.id}>" + "\n" + await bt.styles())
 
-        if message.content.lower().startswith('!tor randchar'):         
+        if message.content.lower().startswith('!tor randchar'):    
+            self.statsDict['!tor randchar'] += 1
             await message.channel.send(f"<@{message.author.id}>" + "\n" + await bt.ranchar())
 
-        if message.content.lower().startswith('!roll'):         
+        if message.content.lower().startswith('!roll'):
+            self.statsDict['!roll'] += 1          
             await message.channel.send(f"<@{message.author.id}>" + "\n" + await dr.parse(message.content))
 
-        if message.content.lower().startswith('!uok'):
-            await message.channel.send(f"<@{244286371181625344}>" + "\n" + "You okay?")
-
         if message.content.lower().startswith('!help'):
+            self.statsDict['!help'] += 1
             await message.channel.send('''Hello! My name is Feyre.
 
 **Commands:**
@@ -79,7 +120,7 @@ class MyClient(discord.Client):
         Ex: !add init Feyre 20
    > !roll #dSize: Rolls any number and types and dice. Supports complicated expressions
         Ex: !roll 1d20 + (5 - 1)/2 + 1d6
-
+   > !stats: Displays number of times each command has been used in the lifetime of the bot
 
    *D&D 5E Specific Commands:*
    > !feat name: Search D&D 5E offical books for a feat (currently only PH). Ex: !feat Keen Mind
@@ -96,6 +137,7 @@ class MyClient(discord.Client):
 Please message <@112041042894655488> if you have any questions/issues.''')
 
         if message.content.lower().startswith('!mm'):
+            self.statsDict['!mm'] += 1
             #search the monster manual
             retArr = await mm.search(message.content.lower())
 
@@ -117,6 +159,7 @@ Please message <@112041042894655488> if you have any questions/issues.''')
                     await message.channel.send(embed = embed)    
 
         if message.content.lower().startswith('!randmonster'):
+            self.statsDict['!randmonster'] += 1
             retArr = await mm.randMonster()
             if(len(retArr[1]) < 2048):
                 embed = discord.Embed(title = retArr[0], description = retArr[1], color=embedcolor)
@@ -136,6 +179,7 @@ Please message <@112041042894655488> if you have any questions/issues.''')
                     await message.channel.send(embed = embed)  
 
         if message.content.lower().startswith('!feat'):
+            self.statsDict['!feat'] += 1
             retArr = await f.search(message.content.lower())
             if(len(retArr[1]) < 2048):
                 embed = discord.Embed(title = retArr[0], description = retArr[1], color=embedcolor)
@@ -155,6 +199,7 @@ Please message <@112041042894655488> if you have any questions/issues.''')
                     await message.channel.send(embed = embed)  
             
         if message.content.lower().startswith('!randfeat'):
+            self.statsDict['!randfeat'] += 1
             retArr = await f.randFeat()
             if(len(retArr[1]) < 2048):
                 embed = discord.Embed(title = retArr[0], description = retArr[1], color=embedcolor)
@@ -174,6 +219,7 @@ Please message <@112041042894655488> if you have any questions/issues.''')
                     await message.channel.send(embed = embed)  
             
         if message.content.lower().startswith('!start init'):
+            self.statsDict['!start init'] += 1
             key = message.guild.name + ":" + message.channel.name
             i = Initiative.Initiative()
             self.initDict[key] = i
@@ -182,28 +228,36 @@ Please message <@112041042894655488> if you have any questions/issues.''')
             self.initEmbedDict[key] = msg
 
         if message.content.lower().startswith('!add init'):
+            self.statsDict['!add init'] += 1
             key = message.guild.name + ":" + message.channel.name
             if(key in self.initDict):
                 split = message.content.split(' ')
                 name = ""
                 init = ""
 
+                #!add init
                 if(len(split) == 2):
                     name = message.author.name
                     init = random.randint(1, 20)
 
+                #!add init something
                 if(len(split) == 3):
+                    #something = int so name = author
                     if (split[2].lstrip('+-').isdigit()):
                         init = split[2]
                         name = message.author.name
+                    #something = name so int = random
                     else:
                         name = split[2]
                         init = random.randint(1, 20)
 
+                #!add init name init OR init name
                 if(len(split) == 4):
+                    #!add init Name Init
                     if (split[3].lstrip('+-').isdigit()):
                         init = split[3]
                         name = split[2]
+                    #!add init Init Name
                     else:
                         init = split[2]
                         name = split[3]
@@ -213,14 +267,25 @@ Please message <@112041042894655488> if you have any questions/issues.''')
                 desc = self.initDict[key].displayInit()
                 newEmbed = discord.Embed(title = "**--------Initiative--------**", description = self.initDict[key].displayInit(), color=embedcolor)
 
+                #delete old message and send new one with updated values
                 self.initEmbedDict[key] = await  self.initEmbedDict[key].delete()
                 self.initEmbedDict[key] = await message.channel.send(embed = newEmbed)
 
             else:
                 await message.channel.send(f"<@{message.author.id}>" + "\n" + "*Please start initiative with !start init before adding players*")
 
+        if message.content.lower().startswith('!stats'):
+            await message.channel.send(await self.displayStats())
+
         if message.content.lower().startswith('!quit'):
             if(message.author.id == 112041042894655488):
+                pyDir = os.path.dirname(__file__)
+                relPath = "_data//stats.txt"
+                absRelPath = os.path.join(pyDir, relPath)
+                with open(absRelPath, 'w') as file:
+                    file.write(json.dumps(self.statsDict))
+
+
                 await message.channel.send("<@112041042894655488> *Shutting down*")
                 await client.close()
                 sys.exit()
@@ -239,4 +304,4 @@ f = Feats.Feats()
 
 embedcolor = discord.Color.from_rgb(165,87,249)
 client = MyClient()
-client.run("NTAwNzMzODQ1ODU2MDU5NDAy.DqPI-g.BBe4LLMfN3QFmrZXOK3mTNwSpws")
+client.run(testToken)
