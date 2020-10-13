fa2d9b00519db8af92e9f941004d9e9ee8277cff
Ian Caragol <iancaragol@gmail.com>
2020-10-12 23:34:01 -0600

    Die.py works with simple expressions

--- Die.py
+++ Die.py
@@ -1,5 +1,6 @@
 # 1d20>15 then 1d8 count 12
 import sys
+from asteval import Interpreter
 from random import randint
 
 class DiceParseError(Exception):
@@ -47,11 +48,7 @@ class Die:
                 for i in range(len(self.rolls) - mod[1]):
                     self.dropped.append(self.rolls.pop(self.rolls.index(min(self.rolls))))
                 
-                
-
-        total = sum(self.rolls)
-
-        self.result = total
+        self.result = sum(self.rolls)
         return self.result
 
 class DiceExpressionEvaluator:
@@ -102,9 +99,12 @@ class DiceExpressionEvaluator:
         dice_list = [] 
         i = 0
         for j in range(len(self.expression_list)): # Because pyhton is weird, our iterator is actually i
+            delete_previous_char = True # If true, then the previous char will be treated as part of the dice. False if previous is +, /, -, etc...
+
             if i >= len(self.expression_list):
                 break
 
+            # DICE
             if self.expression_list[i] == 'd':
                 size = 0
                 number = 0
@@ -116,7 +116,7 @@ class DiceExpressionEvaluator:
                 # First check if number of dice is valid
                 try:
                     size = int(self.expression_list[i+1])
-                except Exception as e:
+                except:
                     raise DiceParseError(self.expression, "Invalid dice size", f"The value {self.expression_list[i+1]} at index {i+1} is not a valid dice size because it is not an integer. \n\n{self.expression_list}")
 
                 # Now check the number of dice
@@ -131,16 +131,18 @@ class DiceExpressionEvaluator:
                             number = float(self.expression_list[i-1]) # If its a float, raise an error
                         except:
                             number = 1 # Otherwise treat number as 1
+                            delete_previous_char = False
                         if type(number) is float:
                             raise DiceParseError(self.expression, "Invalid number of dice", f"{number} is not a valid number of dice because it is not an integer. \n\n{self.expression_list}")
 
                 if i > 0:            
-                    if len(dice_list) > 0:
+                    if len(dice_list) > 0 and delete_previous_char:
                         del dice_list[len(dice_list)-1]
 
                 i += 1 # Skip over next value
                 dice_list.append(Die(number, size)) # This tuple is a die
 
+            # KEEP Modifier
             elif self.expression_list[i] == 'k':
                 keep_count = sys.maxsize # By default, keep all dice
                 
@@ -151,18 +153,19 @@ class DiceExpressionEvaluator:
                 # First check if the keep number is valid
                 try:
                     keep_count = int(self.expression_list[i+1])
-                except Exception as e:
+                except:
                     raise DiceParseError(self.expression, "Invalid keep count", f"The value {self.expression_list[i+1]} at index {i+1} is not a valid number of dice to keep because it is not an integer. \n\n{self.expression_list}")
 
                 if len(dice_list) > 0:
                     if isinstance(dice_list[len(dice_list)-1], Die): # Make sure last value was a dice
                         dice_list[len(dice_list)-1].modifiers.append(('keep', keep_count)) # This tuple is a keep flag
                         i += 1 #Skip next value
-                    else:
-                        raise Exception # Raise exception for a k without a dice
+                    else: # keep without a dice before it
+                        raise DiceParseError(self.expression, "Invalid keep position", f"The keep flag at index {i+1} does directly follow a dice expression. \n\n{self.expression_list}")
                 else:
-                    raise IndexError # Same here
+                    raise DiceParseError(self.expression, "Invalid keep position", f"A dice expression cannot start with a keep flag.\n\n{self.expression_list}")
 
+            #OTHER MODIFIERS GO HERE
 
             else:
                 dice_list.append(self.expression_list[i]) # Add it to expression
@@ -172,13 +175,31 @@ class DiceExpressionEvaluator:
         self.dice_list = dice_list
 
     def evaluate_dice_list(self):
+        """
+        Evaluates each dice in the dice list, constructs and evaluates final string
+        """
+        self.eval_expression = ""
         for i in range(len(self.dice_list)):
             if isinstance(self.dice_list[i], Die):
                 roll_result = self.dice_list[i].evaluate()
-                print(roll_result)
-        
-        
-    
+                self.eval_expression += str(roll_result)
+            else:
+                self.eval_expression += self.dice_list[i]
+
+        # Evaluate the constructed string
+        aeval = Interpreter()
+        self.result = aeval.eval(self.eval_expression)
+
+        return self.result
+
+    def evaluate(self):
+        """
+        Calls all helper functions and returns the result of the dice roll
+        """
+        self.seperate_string_number()
+        self.create_dice_list()
+        return self.evaluate_dice_list()
+
     def is_integer(self, n):
         try:
             int(n)
@@ -188,11 +209,9 @@ class DiceExpressionEvaluator:
             return True
 
 def main():
-    test_die = DiceExpressionEvaluator("1d20+5+1d5k2+3+d5")
-    test_die.seperate_string_number()
-    test_die.create_dice_list()
-    test_die.evaluate_dice_list()
-    print(test_die.expression_list)
+    test_die = DiceExpressionEvaluator("5d20k1>15")
+    print(test_die.evaluate())
+    print()
 
 if __name__ == "__main__":
     main()
---
+++ _classes/DiceRolls.py
@@ -0,0 +1,194 @@
+import random
+import asyncio
+import re
+import copy
+import numpy as np
+import math
+from asteval import Interpreter
+
+aeval = Interpreter()
+
+#rolls dice
+#accepts input in the form of !roll #dTYPE ex: !roll 1d20 + 5
+class Roller():
+    async def parse(self, inp, gm = False):
+        """
+        Parses a string of the format !roll #d# +,/,*,- #d# or # ... evaulated 
+        Ex: !roll 5d20 + 1d6 * 2
+        Returns an invalid input message if the input is not recongnized.
+        """
+        #try:
+            #TODO: Fix this spaget
+        advantage = False
+        disadvantage = False
+        inp = inp.lower().strip()
+        inp = inp.replace('\\','')
+        if(inp.startswith('-a')):
+            advantage = True
+            inp = inp.replace('-a', '').strip()
+
+        if(inp.startswith('-d')):
+            disadvantage = True
+            inp = inp.replace('-d', '').strip()
+
+        #check formatting
+        illegal = "abcefghijklmnopqrstuvwxyz!,[]|&"
+        inp = inp.replace(' ', '')
+        m = re.match(r"^((\d*)d(\d*)([-+*/><]?\d*))*", inp)
+    
+        #sanitize inp
+        sanitize = any(c in inp.lower() for c in illegal)
+
+        if(sanitize == True):
+            return "```I'm sorry, there was something I didnt understand about your input. See !help roll for more info```"
+
+        #5d6+1d20
+        ms = re.split(r"([-+*/()><])", m.string)
+        ms = list(filter(None, ms)) #remove any empty strings that could cause issues
+        adv = copy.deepcopy(ms)
+
+        rollExp = copy.deepcopy(ms)
+
+        for i in range(0, len(ms)):
+            if (re.match(r"^((\d*)d(\d*))", ms[i])):
+                split = ms[i].split('d')
+                try:
+                    numDice = int(split[0])
+
+                except ValueError:
+                    numDice = int(1)
+
+                typeDice = int(split[1])
+
+                if(numDice > 100000):
+                    return "Your inp is too big! Maximum number of dice is 100,000"
+
+                if(typeDice > 9223372036854775808):
+                    return ("Your inp is too big! Maximum size is 9,223,372,036,854,775,807")
+
+
+                ms[i] = self.rollDice(numDice, typeDice)
+                if(advantage or disadvantage):
+                    adv[i] = self.rollDice(numDice, typeDice)
+       
+        unEval = copy.deepcopy(ms)
+        if(advantage or disadvantage):
+            unEvalAdv = copy.deepcopy(adv)
+            evalledAdv = adv
+
+        evalled = ms
+        
+        for i in range(0, len(ms)):
+            try:
+                evalled[i] = sum(ms[i])
+            except:
+                continue
+
+        if(advantage or disadvantage):
+            for i in range(0, len(adv)):
+                try:
+                    evalledAdv[i] = sum(adv[i])
+                except:
+                    continue
+            unEvalStrAdv = ''.join(str(e) for e in unEvalAdv)
+            evalStrAdv = ''.join(str(e) for e in evalledAdv)
+            totalAdv = aeval(evalStrAdv)
+            
+        unEvalStr = ''.join(str(e) for e in unEval)
+        evalStr = ''.join(str(e) for e in evalled)
+        rollExpStr = ''.join(str(e) for e in rollExp)
+        total = aeval(evalStr)
+
+        if(advantage or disadvantage):
+            return self.constructReturnStringAdvantage(advantage, disadvantage, rollExpStr, unEvalStr, unEvalStrAdv, total, totalAdv)
+
+        if(not gm):
+            return self.constructReturnString(rollExpStr, unEvalStr, total)
+        if(gm and not advantage or not disadvantage):
+            return self.constructReturnStringNoFormat(rollExpStr, unEvalStr, total)
+        
+
+        #except Exception as e:
+            #return ("*I'm sorry, there was something I didnt understand about your inp.*\n" + str(e))
+
+    def constructReturnStringAdvantage(self, adv, disadv, rES, uES, uES2, t1, t2):
+        """
+        Constructs the return string where rES is the original expression, uES is the expression with all rolls, and t is the total
+        """
+        if(len(uES) > 100 or len(uES2) > 100):
+            uES = "Omitted (# of dice was too large)"
+
+        if(adv):
+            outMsg = f'''```diff
+I interpreted your input as {rES} with advantage.
+Totals: [{t1}] & [{t2}]
+- You rolled [{max(t1, t2)}] with advantage -```'''
+
+            return outMsg
+
+        elif(disadv):
+            outMsg = f'''```diff
+I interpreted your input as {rES} with disadvantage.
+Totals: [{t1}] & [{t2}]
+- You rolled [{min(t1, t2)}] with disadvantage -```'''
+
+            return outMsg
+
+    def constructReturnString(self,rES, uES, t):
+        """
+        Constructs the return string where rES is the original expression, uES is the expression with all rolls, and t is the total
+        """
+        if(len(uES) > 100):
+            uES = "Omitted (# of dice was too large)"
+
+        if(type(t) is bool):
+            if(t):
+                outMsg = f'''```diff
+I interpreted your input as {rES}.
+Rolls: {uES}
+- Ability/Skill Check: Succeeded -```'''
+            else:
+                outMsg = f'''```diff
+I interpreted your input as {rES}.
+Rolls: {uES}
+- Ability/Skill Check: Failed -```'''
+
+        else:
+            outMsg = f'''```diff
+I interpreted your input as {rES}.
+Rolls: {uES}
+- Total: {"%.2g" % t} -```'''
+
+        return outMsg
+
+    def constructReturnStringNoFormat(self,rES, uES, t):
+        """
+        Constructs the return string where rES is the original expression, uES is the expression with all rolls, and t is the total
+        """
+        if(len(uES) > 100):
+            uES = "Omitted (# of dice was too large)"
+
+        if(type(t) is bool):
+            if(t):
+                outMsg = f'''I interpreted your input as {rES}.
+Rolls: {uES}
+[Ability/Skill Check: Succeeded]'''
+            else:
+                outMsg = f'''I interpreted your input as {rES}.
+Rolls: {uES}
+[Ability/Skill Check: Failed]'''
+
+        else:
+            outMsg = f'''
+I interpreted your input as {rES}.
+Rolls: {uES}
+[Total: {"%.2g" % t}]'''
+
+        return outMsg
+
+    def rollDice(self, numDice, typeDice):
+        """
+        Rolls a number of dice (numDice) of type (typeDice) and returns the rolls as a list.
+        """
+        rolls = np.random.randint(1,typeDice+1,numDice, dtype=np.int64)     
+        return list(rolls)
