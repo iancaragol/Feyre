30288b0970a380300a3e5e7d71f1619150c910a5
Grant Birkinbine <grantbirki@github.com>
2021-09-19 11:56:54 -0700

    setting secrets as sensitive

--- terraform/k8s-cluster/variables.tf
+++ terraform/k8s-cluster/variables.tf
@@ -47,16 +47,20 @@ variable "node_disk_size_gb" {
 # Azure Creds
 variable "CLIENT_SECRET" {
   type = string
+  sensitive = true
 }
 
 variable "CLIENT_ID" {
   type = string
+  sensitive = true
 }
 
 variable "TENANT_ID" {
   type = string
+  sensitive = true
 }
 
 variable "SUBSCRIPTION_ID" {
   type = string
+  sensitive = true
 }
--- terraform/k8s/variables.tf
+++ terraform/k8s/variables.tf
@@ -19,16 +19,20 @@ variable "IMAGE_TAG" {
 # Azure Creds
 variable "CLIENT_SECRET" {
   type = string
+  sensitive = true
 }
 
 variable "CLIENT_ID" {
   type = string
+  sensitive = true
 }
 
 variable "TENANT_ID" {
   type = string
+  sensitive = true
 }
 
 variable "SUBSCRIPTION_ID" {
   type = string
+  sensitive = true
 }
