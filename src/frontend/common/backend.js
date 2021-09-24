045964fded69d84403c3f2f406e060a29448b36a
Grant Birkinbine <grantbirki@github.com>
2021-09-23 20:53:45 -0700

    properly order the else if statements in JS

--- src/frontend/common/backend.js
+++ src/frontend/common/backend.js
@@ -10,17 +10,19 @@ async function create_url({protocol = 'http', host = null, path = null, port = 5
     if (host !== null) {
         host = host;
     }
-    else if (process.env.ENVIRONMENT == "dev" && host === null) {
-        // If the environment is dev, use the "name" set via docker-compose
-        host = 'backend';
-    }
     else if (process.env.ENV_KUBE == "true" && host === null) {
+        // If the ENV_KUBE is true, then we are running in a kubernetes environment
+        // Use the K8s service name 'backend.backend'
         host = "backend.backend";
     }
     else if (process.env.ENVIRONMENT == "prod" && host === null) {
         // If the environment is prod, backend.backend as the hostname
         // backend.backend is the name DNS name of the K8s service in AKS
         host = "backend.backend";
+    }
+    else if (process.env.ENVIRONMENT == "dev" && host === null) {
+        // If the environment is dev, use the "name" set via docker-compose
+        host = 'backend';
     }
 
     // Format the URL
