96dd58fefcff1f5aec39fa7d77ee6faa99b4ae8c
Ian <31246255+iancaragol@users.noreply.github.com>
2021-12-11 13:49:18 -0700

    Add basic prometheus and grafana support

--- src/backend/backend_service/app.py
+++ src/backend/backend_service/app.py
@@ -2,14 +2,19 @@ from os import environ
 import sentry_sdk
 
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 from sentry_sdk.integrations.flask import FlaskIntegration
 from backend_service.api.controller.roll_controller import roll_api
 from backend_service.api.controller.stats_controller import stats_api
 from backend_service.api.controller.healthcheck_controller import healthcheck_api
 from backend_service.api.controller.sentry_controller import sentry_api
 
+metrics = PrometheusMetrics.for_app_factory()
+
 def create_app():
     app = Flask(__name__)
+
+    app.env = environ.get("ENV")
     app.register_blueprint(roll_api, url_prefix='/api/backendservice/roll')
     app.register_blueprint(stats_api, url_prefix='/api/backendservice/stats')
     app.register_blueprint(healthcheck_api, url_prefix='/api/backendservice/healthcheck')
@@ -26,10 +31,13 @@ def create_app():
         traces_sample_rate=1.0
     )
 
-    app.env = environ.get("ENV")
+    return app
 
+def setup_metrics(app):
+    metrics.init_app(app)
     return app
 
 def main():
     app = create_app()
-    app.run(host='0.0.0.0', port = 5000)
+    app = setup_metrics(app)
+    app.run(host='0.0.0.0', port = 5000, debug = False)
--- src/backend/sync_service/app.py
+++ src/backend/sync_service/app.py
@@ -1,15 +1,19 @@
 import atexit
-import os
+from os import environ
+import sentry_sdk
 
+from sentry_sdk.integrations.flask import FlaskIntegration
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
-
+from prometheus_flask_exporter import PrometheusMetrics
 from sync_service.api.sync_stats import sync_stats, stats_sync_api
 from sync_service.api.sync_user import sync_users, users_sync_api
 
+metrics = PrometheusMetrics.for_app_factory()
+
 sync_interval_seconds = 7200 # 2 Hours
 
-if not os.environ.get('DB_BYPASS', None):
+if not environ.get('DB_BYPASS', None):
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=sync_stats, trigger="interval", seconds=sync_interval_seconds)
     scheduler.start()
@@ -21,8 +25,24 @@ def create_app():
     app.register_blueprint(users_sync_api, url_prefix='/api/syncservice/users')
     app.register_blueprint(stats_sync_api, url_prefix='/api/syncservice/stats')
 
+    dsn = environ.get("SENTRY_DSN")
+    sentry_sdk.init(
+        dsn=dsn,
+        integrations=[FlaskIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
+
+    return app
+    
+def setup_metrics(app):
+    metrics.init_app(app)
     return app
 
 def main():
     app = create_app()
-    app.run(host='0.0.0.0', port = 5001)
+    app = setup_metrics(app)
+    app.run(host='0.0.0.0', port = 5001, debug = False)
