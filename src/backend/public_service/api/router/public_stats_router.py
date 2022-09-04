e4f263a51f63f0e2a371c999f37a8fc2880f99f2
Ian <31246255+iancaragol@users.noreply.github.com>
2022-09-03 21:00:31 -0600

    Change public API to JSON

--- src/backend/public_service/api/router/public_stats_router.py
+++ src/backend/public_service/api/router/public_stats_router.py
@@ -2,7 +2,7 @@ import traceback
 import logging
 
 from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import Response, JSONResponse
 from json import dumps
 from fastapi.requests import Request
 from pydantic import BaseModel
@@ -31,7 +31,7 @@ async def stats(request: Request):
         logger.info(f"[STATS] Executing Public Metrics Stats Operation")
         logger.info(f"[STATS] StatsOperation was successful")
         if last_stats != None:
-            return Response(content = dumps(last_stats.dict()), status_code = HTTPStatus.OK)
+            return JSONResponse(content = dumps(last_stats.dict()), status_code = HTTPStatus.OK)
         else:
             return Response(status_code=HTTPStatus.OK)
     except Exception as e:
@@ -47,7 +47,7 @@ async def stats(stats: Stats):
         logger.info(f"[STATS] Caching stats")
         global last_stats
         last_stats = stats
-        return Response(content = dumps(last_stats.dict()), status_code = HTTPStatus.OK)
+        return JSONResponse(content = dumps(last_stats.dict()), status_code = HTTPStatus.OK)
     except Exception as e:
         logger.error(f"[STATS] Exception occurred when caching stats. Traceback: {traceback.format_exc()}")
         return Response(content = f"An exception occurred when getting stats.\n{e}\n{traceback.format_exc()}", status_code = HTTPStatus.INTERNAL_SERVER_ERROR)
